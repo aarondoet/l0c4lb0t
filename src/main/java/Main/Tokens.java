bd2f518f8aab31e161b6a33bc29ffdb73cb43b3a
Aaron <42084688+l0c4lh057@users.noreply.github.com>
2019-05-26 00:19:22 +0200

    Delete Tokens.java

--- src/main/java/Main/Tokens.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-public class Tokens {
-
-    public static final String BOT_TOKEN = "nothing to see here";
-    public static final String MYSQL_USERNAME = "nothing to see here";
-    public static final String MYSQL_PASSWORD = "nothing to see here";
-
-}
